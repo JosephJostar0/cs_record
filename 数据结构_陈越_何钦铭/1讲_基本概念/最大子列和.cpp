9543e71ed1655fee478a4b3a01ac21cf1efdf38c
JosephJostar0 <bohanxiong56@gmail.com>
2022-06-07 15:32:15 +0800

    修正代码。。。

--- 数据结构_陈越_何钦铭/1讲_基本概念/最大子列和.cpp
+++ 数据结构_陈越_何钦铭/1讲_基本概念/最大子列和.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 int main() {
     int N, i, left, right, marginL = 0, marginR = 0;
-    double current_sum, sum, next;
+    int current_sum, sum, num[100001];
     scanf("%d", &N);
-    scanf("%lf", &sum);
-    left = sum > 0 ? 0 : 1, right = 0, marginL = 0, marginR = 0, current_sum = sum > 0 ? sum : 0;
+    for (i = 0; i < N; i++)
+        scanf("%d", num + i);
+    left = num[0] > 0 ? 0 : 1, right = 0, marginL = 0, marginR = 0, current_sum = num[0] > 0 ? num[0] : 0, sum = num[0];
     for (i = 1; i < N; i++) {
-        scanf("%lf", &next);
-        current_sum += next;
+        current_sum += num[i];
         if (current_sum < 0) {
             if (current_sum > sum)
                 sum = current_sum, marginL = left, marginR = i;
@@ -18,6 +18,9 @@ int main() {
         if (current_sum > sum)
             sum = current_sum, marginR = right, marginL = left;
     }
-    printf("sum:%.2f left:%d right:%d", sum, marginL, marginR);
+    if (sum >= 0)
+        printf("%d %d %d", sum, num[marginL], num[marginR]);
+    else
+        printf("%d %d %d", 0, num[0], num[N - 1]);
     return 0;
 }
