a23c83710724f6c015a3102742d9d907d589f76f
JosephJostar0 <bohanxiong56@gmail.com>
2022-07-09 18:31:43 +0800

    奶奶滴，跟我玩阴滴是吧

---
+++ 晴问算法/数据结构专题(2)/堆/堆排序.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+void downAdjust(int heap[], int low, int high) {
+    int dad = low, son = 2 * low;
+    while (son <= high) {
+        if (son + 1 <= high && heap[son] < heap[son + 1])
+            son++;
+        if (heap[dad] >= heap[son])
+            return;
+        std::swap(heap[dad], heap[son]);
+        dad = son;
+        son = dad * 2;
+    }
+}
+
+void createHeap(int heap[], int low, int high) {
+    for (int i = high / 2; i >= low; i--)
+        downAdjust(heap, i, high);
+}
+
+void heapSort(int heap[], int low, int high) {
+    createHeap(heap, low, high);
+    for (int i = high; i > 1; i--) {
+        std::swap(heap[i], heap[1]);
+        downAdjust(heap, 1, i - 1);
+    }
+}
+
+int main() {
+    int n, heap[1003], i;
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+        scanf("%d", heap + i);
+    heapSort(heap, 1, n);
+    printf("%d", heap[1]);
+    for (i = 2; i <= n; i++)
+        printf(" %d", heap[i]);
+    return 0;
+}
