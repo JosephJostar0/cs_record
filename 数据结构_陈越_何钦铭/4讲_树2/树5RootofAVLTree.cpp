4a7981b87ae2414790e2ab5ebe2217667d15a7b4
JosephJostar0 <bohanxiong56@gmail.com>
2022-07-14 15:49:49 +0800

    avl课后习题

---
+++ 数据结构_陈越_何钦铭/4讲_树2/树5RootofAVLTree.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+class Node {
+public:
+    int data, height;
+    Node *left, *right;
+    Node() { height = 1, left = NULL, right = NULL; }
+};
+
+int getHeight(Node *root) {
+    if (!root)
+        return 0;
+    return root->height;
+}
+
+int getBalanceFactor(Node *root) { return getHeight(root->left) - getHeight(root->right); }
+
+void updateHeight(Node *root) { root->height = std::max(getHeight(root->left), getHeight(root->right)) + 1; }
+
+void lTurn(Node *&root) {
+    if (!root || !root->right)
+        return;
+    Node *temp = root->right;
+    root->right = temp->left;
+    temp->left = root;
+    updateHeight(root);
+    updateHeight(temp);
+    root = temp;
+}
+
+void rTurn(Node *&root) {
+    if (!root || !root->left)
+        return;
+    Node *temp = root->left;
+    root->left = temp->right;
+    temp->right = root;
+    updateHeight(root);
+    updateHeight(temp);
+    root = temp;
+}
+
+void insert(Node *&root, int value) {
+    if (!root) {
+        root = new Node;
+        root->data = value;
+        return;
+    }
+    if (value < root->data) {
+        insert(root->left, value);
+        updateHeight(root);
+        if (getBalanceFactor(root) == 2) {
+            if (getBalanceFactor(root->left) == 1)
+                rTurn(root);
+            else
+                lTurn(root->left), rTurn(root);
+        }
+    } else {
+        insert(root->right, value);
+        updateHeight(root);
+        if (getBalanceFactor(root) == -2) {
+            if (getBalanceFactor(root->right) == -1)
+                lTurn(root);
+            else
+                rTurn(root->right), lTurn(root);
+        }
+    }
+}
+
+int main() {
+    int n, i, t;
+    scanf("%d\n%d", &n, &i);
+    Node *root = new Node;
+    root->data = i;
+    for (i = 1; i < n; i++) {
+        scanf("%d", &t);
+        insert(root, t);
+    }
+    printf("%d", root->data);
+    return 0;
+}
