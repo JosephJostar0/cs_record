86075206e344f4dfcce98170c5a25898638a5c84
JosephJostar0 <bohanxiong56@gmail.com>
2022-07-19 21:35:54 +0800

    改进一点

--- 数据结构_陈越_何钦铭/6讲_图1/图3六度空间.cpp
+++ 数据结构_陈越_何钦铭/6讲_图1/图3六度空间.cpp
@@ -4,25 +4,21 @@
 int map[1001][1001], N;
 std::vector<int> v;
 
-int bfs(std::queue<int> q, int step = 0) {
-    if (step > 5)
-        return 0;
-    std::queue<int> q1;
-    int t, i;
-    while (!q.empty()) {
-        t = q.front(), q.pop();
-        for (i = 1; i <= N; i++)
-            if (!v[i] && map[t][i])
-                q1.push(i), v[i] = 1;
-    }
-    return q1.size() + bfs(q1, step + 1);
-}
-
 int getNum(int id) {
     std::queue<int> q;
     q.push(id);
     v[id] = 1;
-    return 1 + bfs(q);
+    int last = id, tail, step = 0, ans = 1, t, i;
+    while (!q.empty()) {
+        t = q.front(), q.pop();
+        for (i = 1; i <= N; i++)
+            if (!v[i] && map[t][i])
+                v[i] = 1, tail = i, q.push(i), ans++;
+        if (t == last)
+            last = tail, step++;
+        if (step > 5)
+            return ans;
+    }
 }
 
 int main() {
