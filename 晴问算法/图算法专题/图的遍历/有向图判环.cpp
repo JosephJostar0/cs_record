06ee9fe24a08d2d93e21af7e1506560e402fc818
JosephJostar0 <bohanxiong56@gmail.com>
2022-07-22 14:19:10 +0800

    有向图判环

---
+++ 晴问算法/图算法专题/图的遍历/有向图判环.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <iostream>
+int n, m, dis[100][100], visit[100], tag;
+
+bool isCircle(int id) {
+    visit[id] = 0;
+    for (int i = 0; i < n; i++)
+        if (dis[id][i]) {
+            if (visit[i] == -1 && isCircle(i))
+                return true;
+            else if (visit[i] == 0)
+                return true;
+        }
+    visit[id] = 1;
+    return false;
+}
+
+int main() {
+    scanf("%d%d", &n, &m);
+    int i, x, y;
+    std::fill(visit, visit + n, -1);
+    for (i = 0; i < m; i++) {
+        scanf("%d%d", &x, &y);
+        dis[x][y] = 1;
+    }
+    for (i = 0; i < n; i++)
+        if (visit[i] == -1 && isCircle(i))
+            break;
+    std::cout << (i != n ? "Yes" : "No");
+    return 0;
+}
