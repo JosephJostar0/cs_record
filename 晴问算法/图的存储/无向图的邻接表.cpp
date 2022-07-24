a4071f36f18091bf5983d9cb1d1d12272e963f39
JosephJostar0 <bohanxiong56@gmail.com>
2022-07-24 14:13:25 +0800

    无向图的邻接表

---
+++ 晴问算法/图的存储/无向图的邻接表.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+std::vector<int> link[100];
+int main() {
+    int n, m, i, x, y;
+    scanf("%d%d", &n, &m);
+    for (i = 0; i < m; i++) {
+        scanf("%d%d", &x, &y);
+        link[x].push_back(y);
+        link[y].push_back(x);
+    }
+    for (i = 0; i < n; i++) {
+        printf("%d(%d)", i, link[i].size());
+        for (x = 0; x < link[i].size(); x++)
+            printf(" %d", link[i][x]);
+        printf("\n");
+    }
+    return 0;
+}
