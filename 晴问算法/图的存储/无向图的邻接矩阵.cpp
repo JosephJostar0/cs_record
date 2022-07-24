78ec9f9bf7f8c8b1a952b68f428e59602236d3f1
JosephJostar0 <bohanxiong56@gmail.com>
2022-07-24 14:08:39 +0800

    无向图的邻接表

---
+++ 晴问算法/图的存储/无向图的邻接矩阵.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int map[100][100];
+int main() {
+    int n, m, i, x, y;
+    scanf("%d%d", &n, &m);
+    for (i = 0; i < m; i++) {
+        scanf("%d%d", &x, &y);
+        map[x][y] = map[y][x] = 1;
+    }
+    for (x = 0; x < n; x++) {
+        printf("%d", map[x][0]);
+        for (y = 1; y < n; y++)
+            printf(" %d", map[x][y]);
+        printf("\n");
+    }
+    return 0;
+}
