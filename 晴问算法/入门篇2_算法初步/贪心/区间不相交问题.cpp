73c6cd5c33f6044e770ff057638c913e90975f44
JosephJostar0 <bohanxiong56@gmail.com>
2022-07-30 15:05:57 +0800

    区间不相交问题

---
+++ 晴问算法/入门篇2_算法初步/贪心/区间不相交问题.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <iostream>
+class Space {
+public:
+    int l, r;
+} space[10000];
+
+bool cmp(const Space a, const Space b) {
+    if (a.l != b.l)
+        return a.l > b.l;
+    return a.r < b.r;
+}
+
+int main() {
+    int n, i, ans = 0, margin = 100000;
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+        scanf("%d%d", &space[i].l, &space[i].r);
+    std::sort(space, space + n, cmp);
+    for (i = 0; i < n; i++)
+        if (space[i].r <= margin)
+            margin = space[i].l, ans++;
+    printf("%d", ans);
+    return 0;
+}
