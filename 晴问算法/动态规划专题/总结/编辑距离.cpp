d3195e666e1ef8bf6e02cfaabaaaf1984b109d2a
JosephJostar0 <bohanxiong56@gmail.com>
2022-08-10 19:15:57 +0800

    编辑距离_递推，简化

--- 晴问算法/动态规划专题/总结/编辑距离.cpp
+++ 晴问算法/动态规划专题/总结/编辑距离.cpp
@@ -7,17 +7,17 @@ int dp[MAXN][MAXN];
 int DP(int x, int y) {
     if (!x || !y || dp[x][y])
         return dp[x][y];
-    int cnt0 = 1 + std::min(DP(x - 1, y), DP(x, y - 1));
+    int cnt0 = 1 + min(DP(x - 1, y), DP(x, y - 1));
     int cnt1 = DP(x - 1, y - 1) + (s1[x - 1] == s2[y - 1] ? 0 : 1);
-    dp[x][y] = std::min(cnt0, cnt1);
+    dp[x][y] = min(cnt0, cnt1);
     return dp[x][y];
 }
 
 int main() {
     cin.tie(NULL);
     cin.sync_with_stdio(false);
-    int i, j;
-    std::cin >> s1 >> s2;
+    int i;
+    cin >> s1 >> s2;
     for (i = 0; i <= s1.size(); i++)
         dp[i][0] = i;
     for (i = 0; i <= s2.size(); i++)
