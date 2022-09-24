710b4707d2bc3f67ceb37e83084ac89601b1d4a6
JosephJostar0 <bohanxiong56@gmail.com>
2022-09-24 15:35:26 +0800

    模拟原码不恢复除法

---
+++ 计算机组成_华中科技大学/3_运算方法与运算器/模拟原码不恢复余数除法.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+int getSignFlag(unsigned int a, unsigned int b) {
+    int sa = (a >> 31), sb = (b >> 31);
+    return (sa ^ sb);
+}
+
+void outHex(int t) {
+    if (t < 0)
+        printf("1."), t *= -1;
+    else
+        printf("0.");
+    printf("%08x", t);
+}
+
+void myDiv(int a, int b) {
+    int SF = getSignFlag(a, b);
+    int x = std::abs(a), y = std::abs(b);
+    if (x > y)
+        std::swap(x, y), std::swap(a, b);
+    int quotient = 0;
+
+    for (int i = 0, next = -1; i <= 32; i++) {
+        x += next * y;
+        if (x > 0)
+            next = -1, quotient += 1;
+        else if (x < 0)
+            next = 1;
+        else {
+            quotient += 1;
+            while (i < 32)
+                quotient <<= 1, i++;
+            break;
+        }
+        if (i != 32)
+            x <<= 1, quotient <<= 1;
+    }
+    while (x < 0)
+        x += y;
+    y *= SF ? -1 : 1;
+
+    outHex(a), printf(" / "), outHex(b), printf(" = ");
+    outHex(quotient), printf("...");
+    outHex(x), printf("*2^-32");
+}
+
+int main() {
+    int x, y;
+    printf("please enter x and y both of which should be integer, separated by a space:\n");
+    scanf("%d%d", &x, &y);
+    if (x == 0x80000000 || y == 0x80000000 || (std::abs(x) == std::abs(y))) {
+        printf("x or y out of range!");
+        return 0;
+    }
+    myDiv(x, y);
+    return 0;
+}
