131eb3a0eb83ba2641fd93d2f25e4fedad82177c
JosephJostar0 <bohanxiong56@gmail.com>
2023-05-21 22:52:13 +0800

    回旋打印二叉树

---
+++ 计算之魂/2_逆向思考_从递推到递归/s2_3_q2回旋打印二叉树节点.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+class Node {
+public:
+    int data;
+    Node *left, *right;
+    Node() { data = -1, left = NULL, right = NULL; }
+    Node(int d) { data = d, left = NULL, right = NULL; }
+};
+
+Node *make_node(int data) {
+    Node *temp = new Node(data);
+    return temp;
+}
+
+Node *init_tree() {
+    Node *root = make_node(1);
+    root->left = make_node(2);
+    root->right = make_node(3);
+    root->left->left = make_node(4);
+    root->left->right = make_node(5);
+    root->right->left = make_node(6);
+    root->right->left->right = make_node(7);
+    return root;
+}
+
+void cycle_order(Node *root) {
+    if (root == NULL)
+        return;
+
+    bool flag = true;
+    Node *last = root;
+    std::stack<Node *> s;
+    s.push(root);
+    while (!s.empty()) {
+        std::stack<Node *> temp;
+        while (!s.empty()) {
+            Node *current = s.top();
+            s.pop();
+            printf("%d ", current->data);
+
+            Node *p[2] = {current->left, current->right};
+            for (int cnt = 0, i = flag ? 0 : 1; cnt < 2; cnt++, i = (i + 1) % 2)
+                if (p[i])
+                    temp.push(p[i]);
+        }
+        s = temp;
+        flag = !flag;
+    }
+}
+
+int main() {
+    Node *root = init_tree();
+    cycle_order(root);
+    return 0;
+}
