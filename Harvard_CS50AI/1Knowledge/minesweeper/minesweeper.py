979ded5a8ed88bb51b157a01790be476f39506d7
josephjostar0 <bohanxiong56@gmail.com>
2023-07-03 20:29:30 +0800

    minesweeper

--- Harvard_CS50AI/1Knowledge/minesweeper/minesweeper.py
+++ Harvard_CS50AI/1Knowledge/minesweeper/minesweeper.py
@@ -105,27 +105,34 @@ class Sentence():
         """
         Returns the set of all cells in self.cells known to be mines.
         """
-        raise NotImplementedError
+        if len(self.cells) == self.count:
+            return self.cells.copy()
+        return set()
 
     def known_safes(self):
         """
         Returns the set of all cells in self.cells known to be safe.
         """
-        raise NotImplementedError
+        if self.count == 0:
+            return self.cells.copy()
+        return set()
 
     def mark_mine(self, cell):
         """
         Updates internal knowledge representation given the fact that
         a cell is known to be a mine.
         """
-        raise NotImplementedError
+        if cell in self.cells:
+            self.cells.remove(cell)
+            self.count -= 1
 
     def mark_safe(self, cell):
         """
         Updates internal knowledge representation given the fact that
         a cell is known to be safe.
         """
-        raise NotImplementedError
+        if cell in self.cells:
+            self.cells.remove(cell)
 
 
 class MinesweeperAI():
@@ -182,7 +189,32 @@ class MinesweeperAI():
             5) add any new sentences to the AI's knowledge base
                if they can be inferred from existing knowledge
         """
-        raise NotImplementedError
+
+        # 1 mark the cell as a move that has been made
+        self.moves_made.add(cell)
+
+        # 2 mark the cell as safe
+        self.mark_safe(cell)
+
+        # 3 add a new sentence to the AI's knowledge base
+        cells = set()
+        count_clear = count
+        for i in range(cell[0] - 1, cell[0] + 2):
+            for j in range(cell[1] - 1, cell[1] + 2):
+                if 0 <= i < self.height and 0 <= j < self.width:
+                    if (i, j) in self.moves_made or (i, j) in self.safes:
+                        continue
+                    if (i, j) in self.mines:
+                        count_clear -= 1
+                        continue
+                    cells.add((i, j))
+        self.knowledge.append(Sentence(cells, count_clear))
+
+        # 4 mark any additional cells as safe or as mines
+        self.clean_knowledge()
+        # 5 add any new sentences to the AI's knowledge base
+        while self.inferred_knowledge():
+            self.clean_knowledge()
 
     def make_safe_move(self):
         """
@@ -193,7 +225,9 @@ class MinesweeperAI():
         This function may use the knowledge in self.mines, self.safes
         and self.moves_made, but should not modify any of those values.
         """
-        raise NotImplementedError
+        for cell in self.safes.difference(self.moves_made):
+            return cell
+        return None
 
     def make_random_move(self):
         """
@@ -202,4 +236,45 @@ class MinesweeperAI():
             1) have not already been chosen, and
             2) are not known to be mines
         """
-        raise NotImplementedError
+        wait_step = []
+        for i in range(self.height):
+            for j in range(self.width):
+                cell = (i, j)
+                if cell not in self.moves_made.union(self.mines):
+                    wait_step.append(cell)
+        if len(wait_step) == 0:
+            return None
+
+        return wait_step[random.randint(0, len(wait_step) - 1)]
+
+    def clean_knowledge(self):
+        flag = True
+        while flag:
+            flag = False
+            copy_knowledge = self.knowledge.copy()
+            for sentence in copy_knowledge:
+                if len(sentence.cells) == 0:
+                    self.knowledge.remove(sentence)
+                    continue
+                for temp in sentence.known_mines():
+                    self.mark_mine(temp)
+                    flag = True
+                for temp in sentence.known_safes():
+                    self.mark_safe(temp)
+                    flag = True
+
+    def inferred_knowledge(self):
+        copy_k = self.knowledge.copy()
+        for i in range(len(copy_k)):
+            for j in range(i+1, len(copy_k)):
+                if copy_k[i].cells.issubset(copy_k[j].cells):
+                    copy_k[j].cells = copy_k[j].cells.difference(
+                        copy_k[i].cells)
+                    copy_k[j].count -= copy_k[i].count
+                    return True
+                elif copy_k[j].cells.issubset(copy_k[i].cells):
+                    copy_k[i].cells = copy_k[i].cells.difference(
+                        copy_k[j].cells)
+                    copy_k[i].count -= copy_k[j].count
+                    return True
+        return False
