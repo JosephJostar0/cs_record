4bb197a9c9cb8bb58a9c106c282af3054ef8f30e
josephjostar0 <bohanxiong56@gmail.com>
2023-07-19 18:28:02 +0800

    nim

--- Harvard_CS50AI/4Learning/nim/nim.py
+++ Harvard_CS50AI/4Learning/nim/nim.py
@@ -96,17 +96,16 @@ class NimAI():
         best_future = self.best_future_reward(new_state)
         self.update_q_value(old_state, action, old, reward, best_future)
 
-    def get_q_value(self, state, action):
+    def get_q_value(self, state: list, action):
         """
         Return the Q-value for the state `state` and the action `action`.
         If no Q-value exists yet in `self.q`, return 0.
         """
-        raise NotImplementedError
+        return self.q[(tuple(state), action)] if (tuple(state), action) in self.q else 0
 
     def update_q_value(self, state, action, old_q, reward, future_rewards):
         """
-        Update the Q-value for the state `state` and the action `action`
-        given the previous Q-value `old_q`, a current reward `reward`,
+        Update the Q-value for the state `state` and the action `action` given the previous Q-value `old_q`, a current reward `reward`,
         and an estiamte of future rewards `future_rewards`.
 
         Use the formula:
@@ -115,39 +114,41 @@ class NimAI():
                    + alpha * (new value estimate - old value estimate)
 
         where `old value estimate` is the previous Q-value,
-        `alpha` is the learning rate, and `new value estimate`
-        is the sum of the current reward and estimated future rewards.
+        `alpha` is the learning rate, and `new value estimate` is the sum of the current reward and estimated future rewards.
         """
-        raise NotImplementedError
+        self.q[(tuple(state), action)] = old_q + \
+            self.alpha * (reward + future_rewards - old_q)
 
-    def best_future_reward(self, state):
+    def best_future_reward(self, state: list):
         """
-        Given a state `state`, consider all possible `(state, action)`
-        pairs available in that state and return the maximum of all
-        of their Q-values.
+        Given a state `state`, consider all possible `(state, action)` pairs available in that state and return the maximum of all of their Q-values.
 
-        Use 0 as the Q-value if a `(state, action)` pair has no
-        Q-value in `self.q`. If there are no available actions in
-        `state`, return 0.
+        Use 0 as the Q-value if a `(state, action)` pair has no Q-value in `self.q`. 
+        If there are no available actions in `state`, return 0.
         """
-        raise NotImplementedError
+        actions = Nim.available_actions(state)
+        if len(actions) == 0:
+            return 0
+        return max(self.get_q_value(state, action) for action in actions)
 
-    def choose_action(self, state, epsilon=True):
+    def choose_action(self, state: list, epsilon=True):
         """
         Given a state `state`, return an action `(i, j)` to take.
 
-        If `epsilon` is `False`, then return the best action
-        available in the state (the one with the highest Q-value,
+        If `epsilon` is `False`, then return the best action available in the state (the one with the highest Q-value,
         using 0 for pairs that have no Q-values).
 
-        If `epsilon` is `True`, then with probability
-        `self.epsilon` choose a random available action,
+        If `epsilon` is `True`, then with probability `self.epsilon` choose a random available action,
         otherwise choose the best action available.
 
-        If multiple actions have the same Q-value, any of those
-        options is an acceptable return value.
+        If multiple actions have the same Q-value, 
+        any of those options is an acceptable return value.
         """
-        raise NotImplementedError
+        actions = Nim.available_actions(state)
+        best = max(actions, key=lambda x: self.get_q_value(state, x))
+        if not epsilon or random.random() >= self.epsilon:
+            return best
+        return random.choice(list(actions))
 
 
 def train(n):
@@ -212,8 +213,7 @@ def train(n):
 def play(ai, human_player=None):
     """
     Play human game against the AI.
-    `human_player` can be set to 0 or 1 to specify whether
-    human player moves first or second.
+    `human_player` can be set to 0 or 1 to specify whether human player moves first or second.
     """
 
     # If no player order set, choose human's order randomly
