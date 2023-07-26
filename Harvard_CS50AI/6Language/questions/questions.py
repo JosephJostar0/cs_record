3ec5aa38b4505b8fb744b9393963d096532aec71
josephjostar0 <bohanxiong56@gmail.com>
2023-07-26 17:31:35 +0800

    CS50AI over!

--- Harvard_CS50AI/6Language/questions/questions.py
+++ Harvard_CS50AI/6Language/questions/questions.py
@@ -1,5 +1,10 @@
 import nltk
 import sys
+import pathlib
+import string
+import math
+import os
+import platform
 
 FILE_MATCHES = 1
 SENTENCE_MATCHES = 1
@@ -43,55 +48,91 @@ def main():
         print(match)
 
 
-def load_files(directory):
+def load_files(directory: str) -> dict:
     """
     Given a directory name, return a dictionary mapping the filename of each
     `.txt` file inside that directory to the file's contents as a string.
     """
-    raise NotImplementedError
+    path = pathlib.Path(directory)
+    if not path.exists() or not path.is_dir():
+        raise ValueError('不存在该路径')
+    fdic = dict()
+    for file in path.iterdir():
+        if file.is_file() and file.suffix == '.txt':
+            with open(file, 'r', encoding='utf-8') as f:
+                fdic[file.name] = f.read()
+    return fdic
 
 
-def tokenize(document):
+def tokenize(document: str) -> list:
     """
-    Given a document (represented as a string), return a list of all of the
-    words in that document, in order.
+    Given a document (represented as a string), return a list of all of the words in that document, in order.
 
-    Process document by coverting all words to lowercase, and removing any
-    punctuation or English stopwords.
+    Process document by coverting all words to lowercase, and removing any punctuation or English stopwords.
     """
-    raise NotImplementedError
+    tokens = nltk.word_tokenize(document)
+    tokens = [token.lower()
+              for token in tokens if token not in string.punctuation and token not in nltk.corpus.stopwords.words('english')]
+    return tokens
 
 
-def compute_idfs(documents):
+def compute_idfs(documents: dict) -> dict:
     """
-    Given a dictionary of `documents` that maps names of documents to a list
-    of words, return a dictionary that maps words to their IDF values.
+    Given a dictionary of `documents` that maps names of documents to a list of words, return a dictionary that maps words to their IDF values.
 
-    Any word that appears in at least one of the documents should be in the
-    resulting dictionary.
+    Any word that appears in at least one of the documents should be in the resulting dictionary.
+
+    Recall that the inverse document frequency of a word is defined by taking the natural logarithm of the number of documents divided by the number of documents in which the word appears.
     """
-    raise NotImplementedError
+    idfs = dict()
+    word_set = set()
+    for doc in documents.keys():
+        word_set.clear()
+        for word in documents[doc]:
+            if word not in word_set:
+                if word not in idfs.keys():
+                    idfs[word] = 0
+                word_set.add(word)
+                idfs[word] += 1
+    for word in idfs.keys():
+        idfs[word] = len(documents) / idfs[word]
+        idfs[word] = round(math.log(idfs[word]), 4)
+    return idfs
 
 
-def top_files(query, files, idfs, n):
+def top_files(query: set, files: dict, idfs: dict, n: int) -> list:
     """
-    Given a `query` (a set of words), `files` (a dictionary mapping names of
-    files to a list of their words), and `idfs` (a dictionary mapping words
-    to their IDF values), return a list of the filenames of the the `n` top
-    files that match the query, ranked according to tf-idf.
+    Given a `query` (a set of words), `files` (a dictionary mapping names of files to a list of their words), and `idfs` (a dictionary mapping words to their IDF values), return a list of the filenames of the the `n` top files that match the query, ranked according to tf-idf.
+    Recall that tf-idf for a term is computed by multiplying the number of times the term appears in the document by the IDF value for that term.
     """
-    raise NotImplementedError
+    tf_idfs = dict()
+    for file in files.keys():
+        tf_idfs[file] = 0
+        for word in query:
+            tf_idfs[file] += files[file].count(word) * idfs[word]
+    tf_idfs = sorted(tf_idfs.items(), key=lambda x: x[1], reverse=True)
+    return [tf_idfs[i][0] for i in range(n)]
 
 
-def top_sentences(query, sentences, idfs, n):
+def top_sentences(query: set, sentences: dict, idfs: dict, n: int) -> list:
     """
     Given a `query` (a set of words), `sentences` (a dictionary mapping
-    sentences to a list of their words), and `idfs` (a dictionary mapping words
-    to their IDF values), return a list of the `n` top sentences that match
-    the query, ranked according to idf. If there are ties, preference should
-    be given to sentences that have a higher query term density.
+    sentences to a list of their words), and `idfs` (a dictionary mapping words to their IDF values), return a list of the `n` top sentences that match the query, ranked according to idf. If there are ties, preference should be given to sentences that have a higher query term density.
+
+    Sentences should be ranked according to “matching word measure”: namely, the sum of IDF values for any word in the query that also appears in the sentence. Note that term frequency should not be taken into account here, only inverse document frequency.
+    If two sentences have the same value according to the matching word measure, then sentences with a higher “query term density” should be preferred. Query term density is defined as the proportion of words in the sentence that are also words in the query. For example, if a sentence has 10 words, 3 of which are in the query, then the sentence's query term density is 0.3.
     """
-    raise NotImplementedError
+    def cmp(x):
+        return x[1], sum([1 for word in query if word in x[0]]) / len(sentences[x[0]])
+
+    tf_idfs = dict()
+    for sentence in sentences.keys():
+        tf_idfs[sentence] = 0
+        for word in query:
+            if word in sentences[sentence]:
+                tf_idfs[sentence] += idfs[word]
+    tf_idfs = sorted(tf_idfs.items(), key=cmp, reverse=True)
+    return [tf_idfs[i][0] for i in range(n)]
 
 
 if __name__ == "__main__":
