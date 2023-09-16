96b8f51d39ee5c4c6a11dffb49c4828fe828fdc0
josephjostar0 <bohanxiong56@gmail.com>
2023-09-16 17:13:03 +0800

    Mult

--- Nand2Tetris/nand2tetris/projects/04/mult/Mult.asm
+++ Nand2Tetris/nand2tetris/projects/04/mult/Mult.asm
@@ -10,3 +10,33 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+//  RAM[2] = 0
+//  while RAM[1] > 0:
+//      RAM[2] += RAM[1]
+//      RAM[1] -= 1
+//
+
+    @R2
+    M = 0   //RAM[2] = 0
+
+(LOOP)
+    @R1
+    D = M   //D = RAM[1]
+    @END
+    D;JEQ   //if D == 0: break the loop
+    @R1
+    M = D - 1 //RAM[1] = D - 1
+
+    @R0
+    D = M   //D = RAM[0]
+
+    @R2
+    M = D + M   //RAM[2] += D
+
+    @LOOP
+    0;JEQ
+
+(END)
+    @END
+    0;JMP
