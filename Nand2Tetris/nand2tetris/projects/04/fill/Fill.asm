e8d001f90952bca2e0d88d34ff52cc27210d847e
josephjostar0 <bohanxiong56@gmail.com>
2023-09-16 18:28:54 +0800

    fill

--- Nand2Tetris/nand2tetris/projects/04/fill/Fill.asm
+++ Nand2Tetris/nand2tetris/projects/04/fill/Fill.asm
@@ -12,3 +12,66 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+//  while True:
+//      if not Press_key:
+//          WIGHT_screen()
+//          continue
+//      BLACK_screen()
+
+    @KBD
+    D = A
+    @R0
+    M = D
+    @SCREEN
+    D = A
+    @R0
+    M = M - D //RAM[0] = KBD - SCREEN
+
+(LOOP)
+    @KBD
+    D = M
+    @LIGHT
+    D;JEQ
+
+//DARK
+    @value
+    M = -1
+    @PRINT
+    0;JMP
+
+(LIGHT)
+    @value
+    M = 0
+
+(PRINT)
+    @R0
+    D = M
+    @i
+    M = D // i = RAM[0] = KBD - SCREEN
+
+    @SCREEN
+    D = A
+    @addr
+    M = D // addr = SCREEN
+
+    (CHANGE)
+        @i
+        D = M
+        @LOOP
+        D;JEQ // if i == 0: goto LOOP
+
+        @value
+        D = M
+        @addr
+        A = M
+        M = D // RAN[addr] = value
+
+        @i
+        M = M - 1 // i = i - 1
+
+        @addr
+        M = M + 1 // addr = addr + 1
+
+        @CHANGE
+        0;JMP
