8a0dbde951d7dac613067f74b28be2e9a9b2808a
josephjostar0 <bohanxiong56@gmail.com>
2023-10-22 23:13:59 +0800

    JackAnalyzer++

---
+++ Nand2Tetris/projects/10/ConstAnalyzer.py
@@ -0,0 +1,22 @@
+ENCODE = 'utf-8'
+MATCH_COMMENT0 = r'//.*'
+MATCH_COMMENT1 = r'/\*.*?\*/'
+MATCH_STRING = r'^".*"$'
+MATCH_IDENTIFYER = r"^(?!^\d)\w+$"
+MATCH_TOKEN = r'([{},\[\].,;+\-*/&|<>=~\(\)])'
+INT_MIN = 0
+INT_MAX = 32767
+
+# JackTokenizer
+KEYWORD = [
+    'class', 'constructor', 'function', 'method',
+    'field', 'static', 'var', 'int', 'char',
+    'boolean', 'void', 'true', 'false', 'null',
+    'this', 'let', 'do', 'if', 'else', 'while', 'return'
+]
+SYMBOL = {
+    '{': '{', '}': '}', '(': '(', ')': ')', '[': '[', ']': ']',
+    '.': '.', ',': ',', ';': ';', '+': '+', '-': '-', '*': '*',
+    '/': '/', '&': '&amp;', '|': '|', '<': '&lt;', '>': '&gt;',
+    '=': '=', '~': '~',
+}
--- Nand2Tetris/projects/10/JackAnalyzer.py
+++ Nand2Tetris/projects/10/JackAnalyzer.py
@@ -1,16 +1,37 @@
 import sys
 from pathlib import Path
+import re
+from queue import Queue
+import xml.etree.ElementTree as ET
+
+from ConstAnalyzer import *
 
 
 class JackTokenizer:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, inPath: Path) -> None:
+        self.setAll(inPath)
+
+    def setAll(self, inPath: Path, outPath: Path = None):
+        if inPath is None or not inPath.exists():
+            raise FileExistsError(f'{inPath} is not a valid file/dir')
+        self.inPath = inPath
+        if outPath is None:
+            if inPath.is_file():
+                outPath = Path(f'{inPath.parent}/{inPath.stem}T_.xml')
+            elif inPath.is_dir():
+                raise FileExistsError(f"{inPath} isn't a file")
+            else:
+                raise FileNotFoundError(f"File/Dir '{inPath}' does not exist.")
+        self.outPath = outPath
+        self.lines = Queue()
+        self.results = ['<tokens>']
+        self.current = ''
 
     def hasMoreTokens(self) -> bool:
         '''
         Are there more tokens in the input?
         '''
-        pass
+        return not self.lines.empty()
 
     def advance(self):
         '''
@@ -18,13 +39,47 @@ class JackTokenizer:
         This method should be called only if hasMoreTokens is true.
         Initially there is no current token.
         '''
-        pass
+        self.current = self.lines.get()
 
     def tokenType(self):
         '''
         Returns the type of the current token, as a constant.
         '''
-        pass
+        token = self.current
+
+        def isKeyWord():
+            return token in KEYWORD
+
+        def isSymbol():
+            return token in SYMBOL.keys()
+
+        def isIdentifier():
+            return bool(re.match(MATCH_IDENTIFYER, token))
+
+        def isInt():
+            def isInt():
+                try:
+                    int(token)
+                    return True
+                except:
+                    return False
+            return isInt() and INT_MIN <= int(token) <= INT_MAX
+
+        def isString():
+            return bool(re.match(MATCH_STRING, token))
+
+        if isKeyWord():
+            return 'keyword'
+        elif isSymbol():
+            return 'symbol'
+        elif isIdentifier():
+            return 'identifier'
+        elif isInt():
+            return 'integerConstant'
+        elif isString():
+            return 'stringConstant'
+        else:
+            raise ValueError(f'{token} is not valid')
 
     def keyWord(self):
         '''
@@ -61,15 +116,72 @@ class JackTokenizer:
         '''
         pass
 
-    @staticmethod
-    def getJackTokenizer():
-        '''
-        Opens the input .jack file and gets ready to tokenize it.
-        '''
-        pass
+    def readAndGetToken(self):
+        def getToken(content: str) -> list:
+            fragments = re.split(MATCH_TOKEN, content)
+            fragments = [it for it in fragments if it != '']
+            result = []
+            for item in fragments:
+                item = item.strip()
+                if item.startswith('"') and item.endswith('"'):
+                    result.append(item)
+                else:
+                    for it in item.split(' '):
+                        it = it.strip()
+                        if len(it) > 0:
+                            result.append(it)
+
+            # 打开文件以追加写入，如果文件不存在则创建它
+            with open('my_file.txt', 'a') as file:
+                for it in result:
+                    file.write(it+'| ')
+                file.write('\n\n------------------------\n\n')
+
+            return result
+
+        def cleanLines(content: str):
+            content = re.sub(MATCH_COMMENT0, '', content).strip()
+            content = re.sub(MATCH_COMMENT1, '', content, flags=re.DOTALL)
+            for line in content.strip().split('\n'):
+                if len(line) > 0:
+                    for item in getToken(line):
+                        self.lines.put(item)
+
+        inPath = self.inPath
+        if not inPath:
+            raise Exception("File path is not ready to read.")
+        if inPath.is_file():
+            with inPath.open('r', encoding=ENCODE) as file:
+                cleanLines(file.read())
+        elif inPath.is_dir():
+            raise TypeError(f"class VMTranslator_ does not accept a dir.")
+
+    def saveResult(self):
+        self.results.append('</tokens>')
+        with open(self.outPath, 'w', encoding=ENCODE) as file:
+            for line in self.results:
+                file.write(line + '\n')
+
+    def tokenize(self):
+        while self.hasMoreTokens():
+            self.advance()
+            tType = self.tokenType()
+            token = self.current.replace('\"', '')
+            if token in SYMBOL.keys():
+                token = SYMBOL[token]
+            result = f'<{tType}> {token} </{tType}>'
+            self.results.append(result)
+
+    def runTokenizer(self):
+        self.readAndGetToken()
+        self.tokenize()
+        self.saveResult()
 
 
 class CompilationEngine:
+    def __init__(self, inPath: Path):
+        pass
+
     def compileLet():
         '''
         Compiles a let statement.
@@ -131,6 +243,16 @@ class JackAnalyzer:
                 raise FileNotFoundError(f"File/Dir '{inPath}' does not exist.")
         self.outPath = outPath
 
+    def runJackAnalyzer(self):
+        if self.inPath.is_file():
+            JackTokenizer(self.inPath).runTokenizer()
+        elif self.inPath.is_dir():
+            for item in self.inPath.iterdir():
+                if item.is_file() and item.suffix == '.jack':
+                    JackTokenizer(item).runTokenizer()
+        else:
+            raise ValueError(f'{self.inPath} is invalid.')
+
     @staticmethod
     def getJackAnalyzer():
         def getPaths() -> tuple[Path, Path]:
@@ -138,25 +260,16 @@ class JackAnalyzer:
 
             if len(arguments) < 2:
                 raise IndexError("Please provide target file_path argument")
-            if len(arguments) > 3:
+            if len(arguments) > 2:
                 raise Exception("Too Many arguments")
 
             inPath = Path(arguments[1])
             if not inPath.exists():
                 raise FileNotFoundError(f"File/Dir '{inPath}' does not exist.")
-            if inPath.is_file():
-                outPath = Path(f'{inPath.parent}/{inPath.stem}.asm')
-                if len(arguments) == 3:
-                    outPath = Path(arguments[2])
-            elif inPath.is_dir():
-                outPath = inPath / f'{inPath.name}.asm'
-            else:
-                raise FileExistsError(f"File/Dir '{inPath}' does not exist.")
-            return inPath, outPath
+            return inPath
 
-        inPath, outPath = getPaths()
-        return JackAnalyzer(inPath, outPath)
+        return JackAnalyzer(getPaths())
 
 
 if __name__ == '__main__':
-    JackAnalyzer.getJackAnalyzer()
+    JackAnalyzer.getJackAnalyzer().runJackAnalyzer()
