a72d602fd8a4b9df22cc5228fc41957b192fa55e
josephjostar0 <bohanxiong56@gmail.com>
2024-02-27 22:46:51 +0800

    ++

--- 晴问算法/比赛/机试模拟赛/D遗忘之地的回响.cpp
+++ 晴问算法/比赛/机试模拟赛/D遗忘之地的回响.cpp
@@ -1,34 +1,28 @@
 #include <algorithm>
 #include <iostream>
-#include <vector>
 using namespace std;
-typedef long long LL;
 #define MAXN 17
-#define MAXT (LL)(16 * 1e9)
+typedef long long LL;
+int n, arr[MAXN];
+LL ans = 1e12;
+void dfs(int step = 0, LL h1 = 0, LL h2 = 0) {
+    if (step == n) {
+        ans = min(ans, abs(h1 - h2));
+        return;
+    }
+    dfs(step + 1, h1 + arr[step], h2);
+    dfs(step + 1, h1, h2 + arr[step]);
+}
 int main() {
     ios::sync_with_stdio(false);
     cout.tie(NULL);
     cin.tie(NULL);
-
-    LL total = 0, target, arr[MAXN];
-    int i, j, n;
-
+    int i;
     cin >> n;
-    for (i = 0; i < n; i++) {
-        cin >> arr[i];
-        arr[i] += (LL)1e9;
-        total += arr[i];
-    }
-    target = total / 2;
-    vector<bool> dp(MAXT, false);
-    dp[0] = true;
     for (i = 0; i < n; i++)
-        for (j = target; j >= 0; j--)
-            if (j >= arr[i])
-                dp[j] = dp[j] || dp[j - arr[i]];
-    for (i = target; i >= 0; i--)
-        if (dp[i])
-            break;
-    cout << abs(total - 2 * i);
+        cin >> arr[i];
+    sort(arr, arr + n);
+    dfs();
+    cout << ans;
     return 0;
 }
